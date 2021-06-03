8f87d08a8f8ad39102e5a67423d956ca9bd0f964
Robson <61189021+TheRobh12@users.noreply.github.com>
2021-06-03 03:44:50 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,13 +3,13 @@ package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.DTO.request.PersonDTO;
 import one.digitalinnovation.personapi.DTO.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -27,6 +27,11 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 
 }
--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -14,6 +14,6 @@ public interface PersonMapper {
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
 
-    PersonDTO tDTO(Person person);
+    PersonDTO toDTO(Person person);
 
 }
